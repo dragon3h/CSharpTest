db05c9ef0183cf39af41e283ab1137d9ea9a51bb
Max Iliashenko <47112175+MaxIliashenko@users.noreply.github.com>
2024-02-19 11:36:16 -0500

    Add sample url

--- CSharpTest.Server/Services/FileDownloadService.cs
+++ CSharpTest.Server/Services/FileDownloadService.cs
@@ -7,5 +7,7 @@ public class FileDownloadService
     {
         // TODO 09: Download the file asyncronously from the given url to the StorageFolder
         // Use HttpClient for downloading and ensure the method handles exceptions properly.
+        // url example:
+        // https://download.visualstudio.microsoft.com/download/pr/f2b824c4-d65c-406a-9d3c-3fc4c45f402b/54a17e458c67caf84e4117783e1d2c40/aspnetcore-runtime-8.0.2-win-x64.zip
     }
 }
