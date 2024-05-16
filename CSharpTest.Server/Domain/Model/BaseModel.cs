a778b4aeeed08579429d820db85df0f24c32f7b5
dragon3h <dragon3h@gmx.com>
2024-05-16 08:05:39 -0400

    feat: did first 4 tasks

--- CSharpTest.Server/Domain/Model/BaseModel.cs
+++ CSharpTest.Server/Domain/Model/BaseModel.cs
@@ -2,5 +2,5 @@
 
 public abstract class BaseModel
 {
-    int Id { get; set; }
+    public int Id { get; set; }
 }
--- CSharpTest.Server/Infrastructure/Repository/EmployeeRepo.cs
+++ CSharpTest.Server/Infrastructure/Repository/EmployeeRepo.cs
@@ -4,21 +4,63 @@ namespace CSharpTest.Infrastructure.Repository;
 
 public class EmployeeRepo
 {
-    public static List<Employee> Storage = [];
+    private static List<Employee> Storage = [];
 
     // TODO 01: add an employee to the list
-    public Employee AddEmployee(Employee e) => new();
+    public Employee AddEmployee(Employee employee)
+    {
+        if (employee == null)
+        {
+            return null;
+        }
+
+        var existingEmployee = Storage.FirstOrDefault(e => e.Id == employee.Id);
+        if (existingEmployee != null)
+        {
+            return existingEmployee;
+        }
+        
+        Storage.Add(employee);
+
+        return employee;
+    }
     
     // TODO Bonus01: add multiple employees to the list
+    public List<Employee> AddEmployees(List<Employee> employees)
+    {
+        foreach (var employee in employees)
+        {
+            AddEmployee(employee);
+        }
+
+        return employees;
+    }
 
     // TODO 02: get all employees
-    public List<Employee> GetEmployees() => [];
+    public List<Employee> GetEmployees()
+    {
+        return Storage;
+    }
 
     // TODO 03: get an employee by id
-    public Employee GetEmployee(int id) => new();
+    public Employee GetEmployee(int id)
+    {
+        return Storage.FirstOrDefault(e => e.Id == id);
+    }
     
     // TODO 04: change the salary of an employee
-    public Employee ChangeSalary(int id, int newSalary) => new();
+    public Employee ChangeSalary(int id, int newSalary)
+    {
+        var emloyee = GetEmployee(id);
+        if (emloyee == null)
+        {
+            return null;
+        }
+
+        emloyee.Salary = newSalary;
+
+        return emloyee;
+    }
 
     // TODO 05: retrieves the names of employees in the given department whose salary is above given number, ordered by name (use LINQ query)
     public List<Employee> GetEmployeesByDepartmentAndSalary(string department, int salary) => [];
