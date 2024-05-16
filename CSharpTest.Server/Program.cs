d9342aae8a369fdee5a345c540216538c3e0f459
dragon3h <dragon3h@gmx.com>
2024-05-16 06:56:39 -0400

    initial commit

--- CSharpTest.Server/Program.cs
+++ CSharpTest.Server/Program.cs
@@ -1,13 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 builder.Services.AddScoped<EmployeeRepo>();
 // TODO 10: Add the FileDownloadService to the services
 // TODO 13: Add the StringService to the services
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
